06f3de3f1e518dde6799fc703ccca1a813e941c3
NastiaKoval <anasteziyam@gmail.com>
2020-03-20 16:00:50 +0200

    feat: make a decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,51 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrOfTens = [];
+
+    for (let i = 0; i < expr.length / 10; i++) {
+        arrOfTens.push(expr.slice(i*10, i*10+10));
+    }
+
+    let arrWithoutZero = [];
+
+    for (let el of arrOfTens) {
+        let i = 0;
+        while (el[i] == '0') {
+            i++;
+        }
+        arrWithoutZero.push(el.slice(i));
+    }
+
+    let arrOfCode = [];
+
+    for (let el of arrWithoutZero) {
+        let code = '';
+        if (el == '**********') {
+            arrOfCode.push(' ');
+        } else {
+            for (let i = 0; i < el.length / 2; i++) {
+                if (el.slice(i*2, i*2+2) == '10') {
+                    code += '.';
+                } else if (el.slice(i*2, i*2+2) == '11') {
+                    code += '-';
+                }
+            }
+            arrOfCode.push(code);
+        }  
+    }
+
+    let result = [];
+
+    for (let el of arrOfCode) {
+        if (el == ' ') {
+            result.push(el);
+        } else {
+            result.push(MORSE_TABLE[el]);
+        }  
+    }
+
+    return result.join('');
 }
 
 module.exports = {
